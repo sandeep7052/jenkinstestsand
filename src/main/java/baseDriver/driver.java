2a1d14649e8b8af27bb0015c57c06a990247261b
sandeep <sandyshukla7052@gmail.com>
2024-10-28 22:46:08 +0530

    changed config on 28

--- src/main/java/baseDriver/driver.java
+++ src/main/java/baseDriver/driver.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class driver {
